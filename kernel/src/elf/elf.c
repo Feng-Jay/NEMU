00cfe27d21ed850c56667ef2c569d123d42af2d9
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 02:52:51 -0800

    >  compile NEMU 3019244196 legend Linux ubuntu 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:52:51 up 39 min,  1 user,  load average: 1.45, 1.42, 1.34 11d518f5a926b6a7fbb576ac7897a3c6ea7577a

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,24 +31,28 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x46454c7f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	int i;
+	ph = (void*)(elf -> e_phoff+buff);
+	for(i = 0; i < elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			ramdisk_read((void*)(ph->p_vaddr),ph->p_offset,ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+			memset((void*)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			ph++;
+
 
 
 #ifdef IA32_PAGE
