43c4159c59f0f3ae0489e919b6db05dd44beaeaa
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-13 17:43:33 -0800

    >  compile NEMU 3019244196 legend Linux ubuntu 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:43:33 up 33 min,  1 user,  load average: 0.09, 0.20, 0.21 2fd8ffdf492dba739f349fa86b10064cdaaf90e4

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -27,6 +27,36 @@ void ini_cache()
     used_time=0;
 }
 
+int read_cache1(hwaddr_t address)
+{
+    uint32_t group_id=(address>>L1cache_bit_blockoffset)&(L1cache_group_number-1);
+    uint32_t tag_id=(address >> (L1cache_bit_blockoffset+L1cache_bit_group));
+
+    int group_address=group_id* L1cache_way_number;
+
+    int i;
+    for(i=group_address;i<(group_address+L1cache_way_number);i++){
+        if(cache1[i].valid==1&&cache1[i].tag==tag_id)
+        {
+            /*hit cache1!*/
+            #ifndef TEST
+            used_time += 2;
+            #endif 
+            return i;
+        }
+    }
+    /*Not hit, we must replace one block */
+    int newblock=read_cache2(address);
+    srand((unsigned int)time(NULL));
+    int victimblock=(rand()%L1cache_way_number)+group_address;
+    /*cache1[victimblock].block=cache2[newblock].block;*/
+    memcpy(cache1[victimblock].block,cache2[newblock].block,L1cache_block_size);
+    cache1[victimblock].valid=1;
+    cache1[victimblock].tag=tag_id;
+    return victimblock;
+
+}
+
 int read_cache2(hwaddr_t address)
 {
     uint32_t group_id=(address>>L2cache_bit_blockoffset)&(L2cache_group_number-1);
@@ -75,66 +105,8 @@ int read_cache2(hwaddr_t address)
     return victimblock;
 }
 
-int read_cache1(hwaddr_t address)
-{
-    uint32_t group_id=(address>>L1cache_bit_blockoffset)&(L1cache_group_number-1);
-    uint32_t tag_id=(address >> (L1cache_bit_blockoffset+L1cache_bit_group));
 
-    int group_address=group_id* L1cache_way_number;
 
-    int i;
-    for(i=group_address;i<(group_address+L1cache_way_number);i++){
-        if(cache1[i].valid==1&&cache1[i].tag==tag_id)
-        {
-            /*hit cache1!*/
-            #ifndef TEST
-            used_time += 2;
-            #endif 
-            return i;
-        }
-    }
-    /*Not hit, we must replace one block */
-    int newblock=read_cache2(address);
-    srand((unsigned int)time(NULL));
-    int victimblock=(rand()%L1cache_way_number)+group_address;
-    /*cache1[victimblock].block=cache2[newblock].block;*/
-    memcpy(cache1[victimblock].block,cache2[newblock].block,L1cache_block_size);
-    cache1[victimblock].valid=1;
-    cache1[victimblock].tag=tag_id;
-    return victimblock;
-
-}
-
-void write_cache2(hwaddr_t addr, size_t len, uint32_t data)/*write back starget!!!!*/
-{
-    uint32_t group=(addr>>L2cache_bit_blockoffset)&(L2cache_group_number-1);
-    uint32_t tag=(addr>>(L2cache_bit_blockoffset+L2cache_bit_group));
-    uint32_t offset=(addr&(L2cache_block_size-1));
-
-    int start_add=group* L2cache_way_number;
-    int i;
-    for(i=start_add+0;i<start_add+L2cache_way_number;i++){
-        /*write hit*/
-        if(cache2[i].valid==1&&cache2[i].tag==tag)
-        {
-            cache2[i].dirty=1;
-            if(offset+len>L2cache_block_size)
-            {
-                memcpy(cache2[i].block+offset,&data,L2cache_block_size-offset);
-                write_cache2(addr+L2cache_block_size-offset, len-L2cache_block_size+offset,data>>(L2cache_block_size-offset));
-            }
-            else
-            {
-                memcpy(cache2[i].block+offset,&data,len);
-            }
-            return ;
-        }
-    }
-     /*if not hit, you must read it from RAM then rewrite it.*/
-     i=read_cache2(addr);
-     cache2[i].dirty=1;
-     memcpy(cache2[i].block+offset,&data,len);
-}
 
 /*easy code with write through*/
 void write_cache1(hwaddr_t addr, size_t len, uint32_t data)
@@ -171,4 +143,35 @@ void write_cache1(hwaddr_t addr, size_t len, uint32_t data)
     }
     /*not hit, then go to L2cache to look for*/
     write_cache2(addr,len,data);
+}
+
+void write_cache2(hwaddr_t addr, size_t len, uint32_t data)/*write back starget!!!!*/
+{
+    uint32_t group=(addr>>L2cache_bit_blockoffset)&(L2cache_group_number-1);
+    uint32_t tag=(addr>>(L2cache_bit_blockoffset+L2cache_bit_group));
+    uint32_t offset=(addr&(L2cache_block_size-1));
+
+    int start_add=group* L2cache_way_number;
+    int i;
+    for(i=start_add+0;i<start_add+L2cache_way_number;i++){
+        /*write hit*/
+        if(cache2[i].valid==1&&cache2[i].tag==tag)
+        {
+            cache2[i].dirty=1;
+            if(offset+len>L2cache_block_size)
+            {
+                memcpy(cache2[i].block+offset,&data,L2cache_block_size-offset);
+                write_cache2(addr+L2cache_block_size-offset, len-L2cache_block_size+offset,data>>(L2cache_block_size-offset));
+            }
+            else
+            {
+                memcpy(cache2[i].block+offset,&data,len);
+            }
+            return ;
+        }
+    }
+     /*if not hit, you must read it from RAM then rewrite it.*/
+     i=read_cache2(addr);
+     cache2[i].dirty=1;
+     memcpy(cache2[i].block+offset,&data,len);
 }
