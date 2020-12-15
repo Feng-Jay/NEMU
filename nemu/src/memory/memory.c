6cc402f8182ec1cbbcc0dc9168281e9050302a03
3019244196-Li Fengjie <2652925120@qq.com>
2020-12-14 20:04:12 -0800

    12/15/2020

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -43,17 +43,27 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	hwaddr_write(addr, len, data);
 }
 
+lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg){
+	if(cpu.CR0.protect_enable == 0){
+		return addr;
+	}else{
+		return cpu.segment_reg[sreg].base+addr;
+	}
+}
+
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	return lnaddr_read(addr, len);
+	lnaddr_t lnaddr=seg_translate(addr,len,sreg);
+	return lnaddr_read(lnaddr, len);
 }
 
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_write(addr, len, data);
+	lnaddr_t lnaddr=seg_translate(addr,len,sreg);
+	lnaddr_write(lnaddr, len, data);
 }
 
