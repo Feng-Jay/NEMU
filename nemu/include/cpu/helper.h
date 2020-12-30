05e202ac7861383ef24f7a982f5548445516db17
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 04:19:15 -0800

    >  compile NEMU 3019244196 legend Linux ubuntu 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:19:15 up  2:20,  1 user,  load average: 0.34, 0.31, 0.32 e37e26333228b060c8a29244968a88a1789223

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -8,9 +8,12 @@
 #define make_helper(name) int name(swaddr_t eip)
 
 static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
-	return swaddr_read(addr, len);
+	uint8_t past_sreg = current_sreg;
+	current_sreg = R_CS;
+	uint32_t res =  swaddr_read(addr, len);
+	current_sreg = past_sreg;
+	return res;
 }
-
 /* Instruction Decode and EXecute */
 static inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {
 	/* eip is pointing to the opcode */
